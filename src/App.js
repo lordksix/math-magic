a45b63b008e931c00e7a85d385620139c80b6a91
lordksix <121689749+lordksix@users.noreply.github.com>
2023-05-30 04:43:48 -0500

    Add buttons object array

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import Calculator from './components/Calculator';
 import './App.css';
+
+const BUTTONS = [
+  { category: 'number', value: 0, id: 0 },
+  { category: 'number', value: 1, id: 1 },
+  { category: 'number', value: 2, id: 2 },
+  { category: 'number', value: 3, id: 3 },
+  { category: 'number', value: 4, id: 4 },
+  { category: 'number', value: 5, id: 5 },
+  { category: 'number', value: 6, id: 6 },
+  { category: 'number', value: 7, id: 7 },
+  { category: 'number', value: 8, id: 8 },
+  { category: 'number', value: 8, id: 8 },
+  { category: 'number', value: 9, id: 8 },
+  { category: 'operation', value: '*', id: 10 },
+  { category: 'operation', value: '+', id: 11 },
+  { category: 'operation', value: '/', id: 12 },
+  { category: 'operation', value: '-', id: 13 },
+  { category: 'operation', value: '=', id: 14 },
+  { category: 'number', value: '±', id: 15 },
+  { category: 'number', value: '%', id: 16 },
+  { category: 'number', value: 'AC', id: 17 },
+  { category: 'number', value: '.', id: 18 },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code> src/App.js </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header" />
+      <Calculator buttons={BUTTONS} />
     </div>
   );
 }
