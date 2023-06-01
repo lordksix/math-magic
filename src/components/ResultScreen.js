af69f075d9400f572ac0e31a57b168ca99980e57
lordksix <121689749+lordksix@users.noreply.github.com>
2023-05-31 22:31:24 -0500

    Add PropTypes and result

--- src/components/ResultScreen.js
+++ src/components/ResultScreen.js
@@ -1,7 +1,26 @@
-const ResultScreen = () => (
+import PropTypes from 'prop-types';
+
+const ResultScreen = ({ totalScreen }) => (
   <div className="screen-calc">
-    0
+    { totalScreen.next || totalScreen.total || '0' }
   </div>
 );
+
+ResultScreen.propTypes = {
+  totalScreen: PropTypes.shape({
+    total: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.oneOf([null]),
+    ]),
+    next: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.oneOf([null]),
+    ]),
+    operation: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.oneOf([null]),
+    ]),
+  }).isRequired,
+};
 
 export default ResultScreen;
