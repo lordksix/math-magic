d02adf62dc2428426454b3c86cb6a3e6dee2feba
lordksix <121689749+lordksix@users.noreply.github.com>
2023-06-01 03:52:26 -0500

    Add REST GET module

---
+++ src/modules/GetAPI.js
@@ -0,0 +1,45 @@
+/**
+ * Helper function for GETing data as JSON with fetch.
+ *
+ * @param {string} url - URL to GET data to
+ * @return {Object} - Response body from URL that was GETed
+ */
+const fetchDataJSON = async (URL) => {
+  const response = await fetch(URL);
+  if (!response.ok) {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
+  }
+  const dataJSON = await response.json();
+  return dataJSON;
+};
+
+/**
+ * Event handler for a form submit event.
+ * @param {SubmitEvent} event
+ */
+const handleGETAPIevent = async (event, url) => {
+  event.preventDefault();
+  try {
+    const responseData = await fetchDataJSON(url);
+    return responseData;
+  } catch (error) {
+    return error;
+  }
+};
+
+/**
+ * Event handler for a form submit event.
+ */
+const handleGETAPI = async (url) => {
+  try {
+    const responseData = await fetchDataJSON(url);
+    return responseData;
+  } catch (error) {
+    return error;
+  }
+};
+
+export {
+  fetchDataJSON, handleGETAPIevent, handleGETAPI,
+};
