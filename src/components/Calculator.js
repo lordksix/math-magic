004359a0beb717fcbb49928105bb647781159ce3
lordksix <121689749+lordksix@users.noreply.github.com>
2023-05-31 22:29:47 -0500

    Add states

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import ResultScreen from './ResultScreen';
 import Keypad from './Keypad';
 
-const Calculator = () => (
-  <div className="calculator-wrapper">
-    <ResultScreen />
-    <Keypad />
-  </div>
-);
+const Calculator = () => {
+  const [calculation, setCalculation] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  return (
+    <div className="calculator-wrapper">
+      <ResultScreen totalScreen={calculation} />
+      <Keypad onClickSetCalculation={setCalculation} calculation={calculation} />
+    </div>
+  );
+};
 
 export default Calculator;
