8fe77f8da3159c2a38f8fda96725850863ed6be2
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-06-06 04:06:08 -0500

    Refactor naming convention

--- src/routes/Calculator.jsx
+++ src/routes/Calculator.jsx
@@ -3,7 +3,7 @@ import CalculatorApp from 'components/Calculator/CalculatorApp';
 const Calculator = () => (
   <>
     <h2>Let&apos;s do some Math</h2>
-    <Calculator />
+    <CalculatorApp />
   </>
 );
 
