5c8805a18c665724012965d1ed85f6b22ba3a3d9
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-06-06 04:03:13 -0500

    Add Calcualtor page

---
+++ src/routes/Calculator.jsx
@@ -0,0 +1,10 @@
+import Calculator from 'components/Calculator/Calculator';
+
+const CalculatorPage = () => (
+  <>
+    <h2>Let&apos;s do some Math</h2>
+    <Calculator />
+  </>
+);
+
+export default CalculatorPage;
