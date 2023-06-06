202d1645c87df90941e68c3f3c733b275921faf5
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-06-06 04:35:01 -0500

    Add navbar component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,10 +2,13 @@ import { Outlet } from 'react-router-dom';
 import Navbar from 'components/Navbar';
 
 const Layout = () => (
-  <div className="wrapper">
-    <Navbar />
+  <>
+    <header className="headerWrapper">
+      <h1>Math Magicians</h1>
+      <Navbar />
+    </header>
     <Outlet />
-  </div>
+  </>
 );
 
 export default Layout;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const links = [
+  { path: '/', text: 'Home' },
+  { path: 'calculator', text: 'Calculator' },
+  { path: 'quote', text: 'Quote' },
+];
+
+const Navbar = () => (
+  <nav className="navbar">
+    <ul>
+      {links.map((link) => (
+        <React.Fragment key={link.text}>
+          <li>
+            <NavLink
+              to={link.path}
+              className={({ isActive }) => (isActive ? 'active' : undefined)}
+            >
+              {link.text}
+            </NavLink>
+          </li>
+        </React.Fragment>
+      ))}
+    </ul>
+  </nav>
+);
+
+export default Navbar;
