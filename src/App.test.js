2b605b55465df4adb5c945f48cbd21a8bea244b7
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-06-07 02:41:54 -0500

    Add testing structures

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Navbar from 'components/Navbar';
-
-test('renders header', () => {
-  render(<Navbar />);
-  const linkElement = screen.getByText(/Math/i);
-  expect(linkElement).toBeInTheDocument();
-});
