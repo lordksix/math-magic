215c4808e3eebaeedcbfbe5c4838abe985bc0985
Amanuel Belete <amanuelbelete95@gmail.com>
2023-06-07 12:14:46 +0300

    Add test for AC button

---
+++ src/logic/__tests__/calculate.test.js
@@ -0,0 +1,14 @@
+import calculate from "logic/calculate";
+
+describe('Calculator Logic', () => {
+   const initialParameter = {total:'10', next:'10', operation:'10'};
+
+  test('AC button', () => {
+    expect(calculate(initialParameter, 'AC')).toEqual({
+      total: null,
+      next: null,
+      operation: null,
+    });
+  });
+
+});
