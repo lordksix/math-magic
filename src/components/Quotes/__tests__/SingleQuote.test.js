ecaabeca30c8eb850ea9b4c99c191976a2d370b2
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-06-07 07:53:40 -0500

    Add SingleQuote component tests

---
+++ src/components/Quotes/__tests__/SingleQuote.test.js
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import SingleQuote from '../SingleQuote';
+
+describe('should render Quote correctly', () => {
+  const itemKey = '1';
+  const itemQuote = 'Once';
+  const itemAuthor = 'Sophie';
+
+  test('should render author', () => {
+    render(<SingleQuote itemKey={itemKey} itemQuote={itemQuote} itemAuthor={itemAuthor} />);
+    const headingElement = screen.getByText(/Sophie/i);
+
+    expect(headingElement).toBeInTheDocument();
+  });
+
+  test('should render quote', () => {
+    render(<SingleQuote itemKey={itemKey} itemQuote={itemQuote} itemAuthor={itemAuthor} />);
+    const headingElement = screen.getByText(/Once/i);
+
+    expect(headingElement).toBeInTheDocument();
+  });
+});
