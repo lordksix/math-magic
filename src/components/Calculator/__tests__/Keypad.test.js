31caee346b0c219ec89323b44106a9b859b8351d
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-06-07 07:22:50 -0500

    Add Keypad tests

---
+++ src/components/Calculator/__tests__/Keypad.test.js
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+import Keypad from '../Keypad';
+
+describe('should render key button', () => {
+  const calculation = { total: null, next: null, operation: null };
+  const setCalculation = () => console.log('test');
+
+  test('should render button 0', () => {
+    render(
+      <Keypad onClickSetCalculation={setCalculation} calculation={calculation} />,
+    );
+    const keyBtnElement = screen.getByText('0');
+
+    expect(keyBtnElement).toBeInTheDocument();
+  });
+
+  test('should render 19 buttons', () => {
+    render(
+      <Keypad onClickSetCalculation={setCalculation} calculation={calculation} />,
+    );
+    const keyBtns = screen.getAllByRole('button');
+
+    expect(keyBtns.length).toBe(19);
+  });
+});
