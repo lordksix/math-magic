a43e6e93c02f6fe52198f45cdaa085dd92bc3ab4
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-06-07 06:16:45 -0500

    Add test to calculate test suites

--- src/logic/__tests__/calculate.test.js
+++ src/logic/__tests__/calculate.test.js
@@ -2,8 +2,11 @@ import calculate from 'logic/calculate';
 
 describe('Calculator Logic', () => {
   const initialParameter = { total: null, next: '10', operation: null };
+  const readyOp = { total: '10', next: '10', operation: '+' };
   const nullObj = { total: null, next: null, operation: null };
   const zeroObj = { total: '0', next: '0', operation: null };
+  const zeroTotal = { total: '0', next: null, operation: null };
+  const operation = '+';
 
   describe('Number option is selected', () => {
     test('Initial value is null', () => {
@@ -31,7 +34,29 @@ describe('Calculator Logic', () => {
     });
   });
 
+  describe('Operation option is selected', () => {
+    test('Initial value is null', () => {
+      expect(calculate(nullObj, operation)).toEqual({ operation });
+    });
+
+    test('There is a initial value', () => {
+      expect(calculate(initialParameter, operation)).toEqual({ total: '10', next: null, operation });
+    });
+
+    test('Total is zero and no initial value', () => {
+      expect(calculate(zeroTotal, operation)).toEqual({ total: '0', next: null, operation });
+    });
+
+    test('Add one operation when one already exists', () => {
+      expect(calculate(readyOp, operation)).toEqual({ total: '20', next: null, operation });
+    });
+  });
+
   test('AC should return all properties null', () => {
     expect(calculate(initialParameter, 'AC')).toEqual(nullObj);
+  });
+
+  test('Equal button is press whe all is ready', () => {
+    expect(calculate(readyOp, '=')).toEqual({ total: '20', next: null, operation: null });
   });
 });
