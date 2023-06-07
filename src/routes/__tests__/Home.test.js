10c7bf7cfa2834a293084f8ea325094be8cc42f2
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-06-07 02:51:15 -0500

    Add test for Home page

---
+++ src/routes/__tests__/Home.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Home from 'routes/Home';
+
+test('Should render heading', async () => {
+  render(<Home />);
+  const headingElement = screen.getByRole('heading', { name: 'Welcome to our page' });
+  expect(headingElement).toBeInTheDocument();
+});
